72e0e08b5e93148a8d33acf72c33e93874f6628f
Emilypybites <emily@pybit.es>
2023-09-16 12:24:39 +1000

    test

---
+++ test.py
@@ -0,0 +1,6 @@
+month = input("What's the name of the 12th month of the calendar year?")
+month = input("What's the name of the 12th month of the calendar year?: ")
+month = input("What's the name of the 12th month of the calendar year? ")
+lowercase_autobot = autobot.lower()
+ninjabelts.append('bronze')
+ninjabelts.remove('blue')
