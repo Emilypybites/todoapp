ebb19eb434202a68d0b5e5837ee9d81d1c39c541
Emilypybites <emily@pybit.es>
2023-09-29 19:44:40 +1000

    todo

--- test.py
+++ test.py
@@ -1,6 +1,45 @@
-month = input("What's the name of the 12th month of the calendar year?")
-month = input("What's the name of the 12th month of the calendar year?: ")
-month = input("What's the name of the 12th month of the calendar year? ")
-lowercase_autobot = autobot.lower()
-ninjabelts.append('bronze')
-ninjabelts.remove('blue')
+todos = []
+
+def add_task():
+    task = input("Enter task:")
+    todos.append(task)
+
+def view_tasks():
+    if not todos:
+        print("No tasks found")
+
+    else:
+        for todo in todos:
+            print(todo)
+
+
+from dataclasses import dataclass
+
+@dataclass
+class Todo:
+    title: str
+    completed: bool
+
+todos = []
+
+def add_todo():
+    user_input = input("Add a task: ")
+    todo = Todo(title=user_input, completed=False)
+    todos.append(todo)
+
+def complete_todo(number):
+    todos[number - 1].completed = True
+
+def print_todos():
+    for index, todo in enumerate(todos, start=1):
+        print(f"{index}. {todo.title} - {todo.completed}")
+
+add_todo()
+add_todo()
+print_todos()
+print()
+complete_todo(1)
+print_todos()
+
+
+
